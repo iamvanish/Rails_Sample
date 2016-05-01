20a6ffa65aedcd254e8c6ba9d9303cb268d015a9
iamvanish <iamvanish@outlook.com>
2016-05-01 13:56:48 +0000

    remember

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "欢迎注册新用户"
       redirect_to @user
     else
       render 'new'
     end
+    
   end
   
   private
