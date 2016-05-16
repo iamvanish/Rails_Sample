bcfd1370b71a0b79201cdfe136b90454ffafe73f
iamvanish <iamvanish@outlook.com>
2016-05-16 12:36:09 +0000

    Finish user edit, update

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   
   has_secure_password
   
-  validates(:password, presence: true, length:{ minimum:6 })
+  validates(:password, presence: true, length:{ minimum:6 }, allow_nil: true )
   
   #返回指定字符串的哈希摘要
   def User.digest(string) 
